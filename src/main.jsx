a90fb2cc9cc91873eac9b641e216548fc899c426
luigizuniga <luigialbertozuniga@gmail.com>
2023-04-11 02:24:19 -0400

    React redux - pokemonApi with react redux and axios

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
-import App from './App'
+import { PokemonApp } from './PokemonApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Provider store={ store }>
-        <App />
+        <PokemonApp />
       </Provider>
   </React.StrictMode>,
 )
